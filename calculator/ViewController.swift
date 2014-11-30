2f4e5cc6f8a9a6cc0551cfae52cf66fa42abca49
Hubert Lee <hubertlee915@gmail.com>
2014-11-30 23:17:28 +0800

    修复了ViewController和Storyboard绑定的bug

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,10 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var expressionField: UITextField!
+    
+    @IBAction func requestResult(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func btnadd(sender: AnyObject) {
+    }
+    
+    @IBAction func btnsubtract(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func btnmultiply(sender: AnyObject) {
+    }
+    
+    @IBAction func btndivide(sender: AnyObject) {
+    }
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func btn1(sender: AnyObject) {
+    }
+    
+    @IBAction func btn2(sender: AnyObject) {
+    }
+    
+    @IBAction func btn3(sender: AnyObject) {
+    }
+    
+    @IBAction func btn4(sender: AnyObject) {
+    }
+    
+    @IBAction func btn5(sender: AnyObject) {
+    }
+    
+    @IBAction func btn6(sender: AnyObject) {
+    }
+    
+    @IBAction func btn7(sender: AnyObject) {
+    }
+    
+    @IBAction func btn8(sender: AnyObject) {
+    }
+    
+    @IBAction func btn9(sender: AnyObject) {
+    }
+    
+    @IBAction func btn0(sender: AnyObject) {
+    }
+    
 
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
